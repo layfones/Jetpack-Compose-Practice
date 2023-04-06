236deba7c02d24dfc9775ab7affdb8c876591fff
layfones <layfones@gmail.com>
2023-04-06 20:50:37 +0800

    添加截图

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Jetpack-Compose-Practice
 Jetpack Compose Wanandroid
 
+#### 截图展示
+----
+
+| 首页                                | 项目                                | 导航                                | 我                                 |
+|-----------------------------------|-----------------------------------|-----------------------------------|-----------------------------------|
+| ![](screenshots/Screenshot_1.jpg) | ![](screenshots/Screenshot_2.jpg) | ![](screenshots/Screenshot_3.jpg) | ![](screenshots/Screenshot_4.jpg) |
+| ![](screenshots/Screenshot_5.jpg) | ![](screenshots/Screenshot_6.jpg) | ![](screenshots/Screenshot_7.jpg) | ![](screenshots/Screenshot_8.jpg) |
+	
+----
+
 ## Thanks:
 - 我心目中页面最漂亮，代码最漂亮的-玩安卓 [Design-WanAndroid](https://github.com/Lowae/Design-WanAndroid)
 - 很棒的 Compose 玩安卓 [wanandroid-compose](https://github.com/RicardoJiang/wanandroid-compose)
--- app/src/main/java/com/layfones/composewanandroid/ui/theme/Theme.kt
+++ app/src/main/java/com/layfones/composewanandroid/ui/theme/Theme.kt
@@ -1,8 +1,74 @@
 package com.layfones.composewanandroid.ui.theme
 
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Light default theme color scheme
+ */
+@VisibleForTesting
+val LightDefaultColorScheme = lightColorScheme(
+    primary = Purple40,
+    onPrimary = Color.White,
+    primaryContainer = Purple90,
+    onPrimaryContainer = Purple10,
+    secondary = Orange40,
+    onSecondary = Color.White,
+    secondaryContainer = Orange90,
+    onSecondaryContainer = Orange10,
+    tertiary = Blue40,
+    onTertiary = Color.White,
+    tertiaryContainer = Blue90,
+    onTertiaryContainer = Blue10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = DarkPurpleGray99,
+    onBackground = DarkPurpleGray10,
+    surface = DarkPurpleGray99,
+    onSurface = DarkPurpleGray10,
+    surfaceVariant = PurpleGray90,
+    onSurfaceVariant = PurpleGray30,
+    inverseSurface = DarkPurpleGray20,
+    inverseOnSurface = DarkPurpleGray95,
+    outline = PurpleGray50,
+)
+
+/**
+ * Dark default theme color scheme
+ */
+@VisibleForTesting
+val DarkDefaultColorScheme = darkColorScheme(
+    primary = Purple80,
+    onPrimary = Purple20,
+    primaryContainer = Purple30,
+    onPrimaryContainer = Purple90,
+    secondary = Orange80,
+    onSecondary = Orange20,
+    secondaryContainer = Orange30,
+    onSecondaryContainer = Orange90,
+    tertiary = Blue80,
+    onTertiary = Blue20,
+    tertiaryContainer = Blue30,
+    onTertiaryContainer = Blue90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = DarkPurpleGray10,
+    onBackground = DarkPurpleGray90,
+    surface = DarkPurpleGray10,
+    onSurface = DarkPurpleGray90,
+    surfaceVariant = PurpleGray30,
+    onSurfaceVariant = PurpleGray80,
+    inverseSurface = DarkPurpleGray90,
+    inverseOnSurface = DarkPurpleGray10,
+    outline = PurpleGray60,
+)
 
 private val DarkColorPalette = darkColorScheme()
 
@@ -19,9 +85,9 @@ fun ComposeWanandroidTheme(
 ) {
 
     val colors = when (theme) {
-        WanandroidTheme.LIGHT -> LightColorPalette
-        WanandroidTheme.DARK -> DarkColorPalette
-        WanandroidTheme.SYSTEM -> if (isSystemInDarkTheme()) DarkColorPalette else LightColorPalette
+        WanandroidTheme.LIGHT -> LightDefaultColorScheme
+        WanandroidTheme.DARK -> DarkDefaultColorScheme
+        WanandroidTheme.SYSTEM -> if (isSystemInDarkTheme()) DarkDefaultColorScheme else LightDefaultColorScheme
     }
 
     MaterialTheme(
